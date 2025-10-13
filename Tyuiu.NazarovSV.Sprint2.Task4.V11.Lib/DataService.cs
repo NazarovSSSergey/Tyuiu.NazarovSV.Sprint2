d5c478167e7410d841b20a7a7a0eeaf01794b3b5
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-10-13 14:36:58 +0500

    Task4

--- Tyuiu.NazarovSV.Sprint2.Task4.V11.Lib/DataService.cs
+++ Tyuiu.NazarovSV.Sprint2.Task4.V11.Lib/DataService.cs
@@ -5,7 +5,12 @@ namespace Tyuiu.NazarovSV.Sprint2.Task4.V11.Lib
     {
         public double Calculate(double x, double y)
         {
-            return Math.Round(((x - 20 * 2 < y / 4 ? Math.Pow((3 + (8 / x * x)), y) : y - Math.Pow(((x + 1) / (y + 2)), x))), 3);
+            double a = (x-20*2);
+            double c = (y / 4);
+            double z = Math.Pow((3 + (8 / x * x)), y);
+            double z2 = y - Math.Pow(((x + 1) / (y + 2)), x);
+            double z3 = a < c ? z : z2;
+            return Math.Round(z3, 3);
         }
     }
 }
