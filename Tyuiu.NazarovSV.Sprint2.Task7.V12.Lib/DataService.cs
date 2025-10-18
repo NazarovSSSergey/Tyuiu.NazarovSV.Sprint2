c1925f95e303afb78093dd7f5118f00e786f2d53
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-10-18 13:34:57 +0500

    Task7

--- Tyuiu.NazarovSV.Sprint2.Task7.V12.Lib/DataService.cs
+++ Tyuiu.NazarovSV.Sprint2.Task7.V12.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.NazarovSV.Sprint2.Task7.V12.Lib
             bool res;
             double pr = Math.Pow(x, 2);
             double pr1 = 2 - x;
-            if ((pr >= pr1 && x < -2 && y > 4) || (pr <= pr1 && x >0 && y <0 && x < 2 && y < 1)) { res = true; }
+            if ((pr >= pr1 && x <= -2 && y >= 4) || (pr <= pr1 && x >= 0 && y >= 0 && x <= 2 && y <= 1)) { res = true; }
             else { res = false; }
             return res;
         }
