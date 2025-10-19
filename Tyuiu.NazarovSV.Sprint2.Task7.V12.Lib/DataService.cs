2354bc5d19ecd6fdd7a744df6f399d124f0ab80b
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-10-19 18:16:22 +0500

    Task7

--- Tyuiu.NazarovSV.Sprint2.Task7.V12.Lib/DataService.cs
+++ Tyuiu.NazarovSV.Sprint2.Task7.V12.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.NazarovSV.Sprint2.Task7.V12.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if (((y <= Math.Pow(x, 2) && y >= (2 - x)) || (y >= Math.Pow(x, 2) && y <=(2-x) )))
+            if (((y <= Math.Pow(x, 2) && y >= (2 - x)) || (y >= Math.Pow(x, 2) && y <=(2-x) ) || (y <= Math.Pow(x, 2) && y <= (2 - x))))
             { 
                 res = true;
             }
